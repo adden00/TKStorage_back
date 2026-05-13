e1c7f2995954fb210a6b5781434b50b9e30ecfbf
addenisov <a.d.denisov@mail.ru>
2026-05-13 18:20:25 +0300

    fix cors

---
+++ src/main/kotlin/com/adden00/TKStorageBack/config/WebConfig.kt
@@ -0,0 +1,15 @@
+package com.adden00.TKStorageBack.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOriginPatterns("*")
+            .allowedMethods("POST")
+            .allowedHeaders("Content-Type")
+    }
+}
