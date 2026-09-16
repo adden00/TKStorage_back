8bde0f97232a64068ea88a3ab2abb4b58ce0fff0
addenisov <a.d.denisov@mail.ru>
2026-09-17 01:40:27 +0300

    bump version

--- src/main/kotlin/com/adden00/tk_storage_back/config/VersionCheckFilter.kt
+++ src/main/kotlin/com/adden00/tk_storage_back/config/VersionCheckFilter.kt
@@ -9,7 +9,7 @@ import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
 import org.springframework.web.filter.OncePerRequestFilter
 
-private const val MIN_VERSION_CODE = 3
+private const val MIN_VERSION_CODE = 4
 
 @Component
 class VersionCheckFilter(private val objectMapper: ObjectMapper) : OncePerRequestFilter() {
